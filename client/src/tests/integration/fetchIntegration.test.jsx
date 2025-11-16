06ada9ca6695fd7b0715134f6e9ea630f0804f52
Flavian6972 <onyangofle@gmail.com>
2025-11-16 21:07:07 +0300

    test(client): add integration test with mocked fetch API

---
+++ client/src/tests/integration/fetchIntegration.test.jsx
@@ -0,0 +1,10 @@
+import { render, screen } from '@testing-library/react';
+import App from '../../App';
+
+
+
+
+test('renders App heading', () => {
+render(<App />);
+expect(screen.getByText('App')).toBeInTheDocument();
+});
